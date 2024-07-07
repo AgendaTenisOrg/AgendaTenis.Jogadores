0ee33b08317c0f106df4695ce7201e2850a4c58a
matheuscm (Matheus Campanini) <matheus.campanini@ploomes.com>
2024-07-06 22:32:31 -0300

    Removidos comentários inúteis no código

--- src/Ports/AgendaTenis.Jogadores.WebApi/Startup.cs
+++ src/Ports/AgendaTenis.Jogadores.WebApi/Startup.cs
@@ -17,7 +17,6 @@ public class Startup
 
     public IConfiguration Configuration { get; }
 
-    // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
         services.AddControllers();
@@ -43,7 +42,6 @@ public class Startup
         });
     }
 
-    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env, JogadoresDbContext jogadoresDbContext)
     {
         if (env.IsDevelopment())
